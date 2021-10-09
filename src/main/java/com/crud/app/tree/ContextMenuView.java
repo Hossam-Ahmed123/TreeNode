db2b14112bd64366eb2ab18509f72ed7a3810a11
Hossam0100 <hossama665@gmail.com>
2021-10-09 14:33:52 +0200

    up

---
+++ src/main/java/com/crud/app/tree/ContextMenuView.java
@@ -0,0 +1,98 @@
+package com.crud.app.tree;
+
+import java.io.Serializable;
+
+import javax.annotation.PostConstruct;
+import javax.faces.application.FacesMessage;
+import javax.faces.context.FacesContext;
+import javax.faces.view.ViewScoped;
+import javax.inject.Inject;
+import javax.inject.Named;
+
+import org.ocpsoft.rewrite.annotation.Join;
+import org.ocpsoft.rewrite.el.ELBeanName;
+import org.primefaces.event.NodeCollapseEvent;
+import org.primefaces.event.NodeExpandEvent;
+import org.primefaces.event.NodeSelectEvent;
+import org.primefaces.event.NodeUnselectEvent;
+import org.primefaces.event.data.FilterEvent;
+import org.primefaces.event.data.SortEvent;
+import org.primefaces.model.TreeNode;
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Component;
+
+@Named("treeContextMenuView")
+ 
+
+@Scope(value = "session")
+ 
+@Join(path = "/", to = "/tree.jsf")
+public class ContextMenuView implements Serializable {
+
+    private TreeNode root;
+
+    private TreeNode selectedNode;
+
+    @Inject
+    private DocumentService service;
+
+    @PostConstruct
+    public void init() {
+        root = service.createDocuments();
+    }
+
+    public TreeNode getRoot() {
+        return root;
+    }
+
+    public TreeNode getSelectedNode() {
+        return selectedNode;
+    }
+
+    public void setSelectedNode(TreeNode selectedNode) {
+        this.selectedNode = selectedNode;
+    }
+
+    public void setService(DocumentService service) {
+        this.service = service;
+    }
+
+    public void displaySelectedSingle() {
+        if (selectedNode != null) {
+            FacesMessage message = new FacesMessage(FacesMessage.SEVERITY_INFO, "Selected", selectedNode.getData().toString());
+            FacesContext.getCurrentInstance().addMessage(null, message);
+        }
+    }
+
+    public void deleteNode() {
+        selectedNode.getChildren().clear();
+        selectedNode.getParent().getChildren().remove(selectedNode);
+        selectedNode.setParent(null);
+
+        selectedNode = null;
+    }
+    
+    
+    public void onNodeExpand(NodeExpandEvent event) {
+        FacesMessage message = new FacesMessage(FacesMessage.SEVERITY_INFO, "Expanded", event.getTreeNode().toString());
+        FacesContext.getCurrentInstance().addMessage(null, message);
+    }
+
+    public void onNodeCollapse(NodeCollapseEvent event) {
+        FacesMessage message = new FacesMessage(FacesMessage.SEVERITY_INFO, "Collapsed", event.getTreeNode().toString());
+        FacesContext.getCurrentInstance().addMessage(null, message);
+    }
+
+    public void onNodeSelect(NodeSelectEvent event) {
+        FacesMessage message = new FacesMessage(FacesMessage.SEVERITY_INFO, "Selected", event.getTreeNode().toString());
+        FacesContext.getCurrentInstance().addMessage(null, message);
+    }
+
+    public void onNodeUnselect(NodeUnselectEvent event) {
+        FacesMessage message = new FacesMessage(FacesMessage.SEVERITY_INFO, "Unselected", event.getTreeNode().toString());
+        FacesContext.getCurrentInstance().addMessage(null, message);
+    }
+
+   
+
+}
